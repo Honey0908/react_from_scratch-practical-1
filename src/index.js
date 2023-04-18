ad7dfed306585bf5d106cc4570bfee5146fee20e
Honey Patel <honey.p@simformsolutions.com>
2023-04-18 11:44:49 +0530

    created react app from scratch using webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function Welcome() {
+
+    return <h1>Hello World !</h1>;
+
+}
+ReactDOM.render(<Welcome />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html", // to import index.html file inside index.js
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3030,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+};
